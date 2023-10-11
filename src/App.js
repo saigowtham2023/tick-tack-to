75f68abe6f639730b93a307d75c6bf32c0740b47
Sai Gowtham Tammineni <tamminenisaigowtham@gmail.com>
2023-10-12 04:01:26 +0530

    taking turns complete

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ function Square({ value, onSquareClick }) {
   );
 }
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquare] = useState(Array(9).fill(null))
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    }
+    else {
+      nextSquares[i] = 'O'
+    }
     setSquare(nextSquares);
+    setXIsNext(!xIsNext)
   }
   return (
     <>
