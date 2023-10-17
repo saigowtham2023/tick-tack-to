73c18d5b3404a47070cd9736cdf492bd74a30258
Sai Gowtham Tammineni <tamminenisaigowtham@gmail.com>
2023-10-17 16:02:25 +0530

    showed elements using list without assignig key

--- src/App.js
+++ src/App.js
@@ -58,14 +58,35 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
   const currentSquares = history[history.length - 1]
+
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares])
     setXIsNext(!xIsNext)
   }
+  function jumpTo(nextmoves) {
+
+  }
+  const moves = history.map((squares, move) => {
+    let description
+    if (move > 0) {
+      description = 'go to move #' + move
+    }
+    else {
+      description = 'go to start'
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(moves)}>{description}</button>
+      </li>
+    );
+  })
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
       </div>
     </div>
   );
